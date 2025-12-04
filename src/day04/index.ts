e1aead01d8904038a479e32e8dcdad351803795f
Luca Schultz <github@lucaschultz.com>
2025-12-04 18:59:26 +0100

    feat: add "recucer hell" solution for day 4 part 2
    Signed-off-by: Luca Schultz <github@lucaschultz.com>

--- src/day04/index.ts
+++ src/day04/index.ts
@@ -19,6 +19,22 @@ function getWarehouseItem(warehouse: Warehouse, position: WarehousePosition): Wa
   return warehouse[position[0]]?.[position[1]] === WarehouseItem.Box ? WarehouseItem.Box : WarehouseItem.Empty
 }
 
+/**
+ * Returns a new warehouse with the item set at the given position
+ * 
+ * @remark This is **not efficient**, but it is a pure function and uses the `map` method as requested by Flo
+ */
+function setWarehouseItem(warehouse: Warehouse, position: WarehousePosition, item: WarehouseItem): Warehouse {
+  return warehouse.map((row, rowIndex) => {
+    return row.map((col, colIndex) => {
+      if (rowIndex === position[0] && colIndex === position[1]) {
+        return item
+      }
+      return col
+    })
+  })
+}
+
 function addPositions(a: WarehousePosition, b: WarehousePosition): WarehousePosition {
   return [a[0] + b[0], a[1] + b[1]]
 }
@@ -44,6 +60,35 @@ function countBoxNeighbors(warehouse: Warehouse, position: WarehousePosition): n
   }, 0)
 }
 
+function removeBoxes(currentWarehouse: Warehouse): number {
+  // this is why using `.reduce` is considered an anti pattern by some developers...
+  const { updatedWarehouse, boxesRemoved } = currentWarehouse.reduce(
+    (acc, row, rowIndex) => {
+      const rowResult = row.reduce(
+        (rowAcc, _, colIndex) => {
+          const position: WarehousePosition = [rowIndex, colIndex]
+          if (getWarehouseItem(rowAcc.warehouse, position) === WarehouseItem.Box &&
+            countBoxNeighbors(rowAcc.warehouse, position) < 4) {
+            return {
+              warehouse: setWarehouseItem(rowAcc.warehouse, position, WarehouseItem.Empty),
+              count: rowAcc.count + 1,
+            }
+          }
+          return rowAcc
+        },
+        { warehouse: acc.updatedWarehouse, count: 0 }
+      )
+      return {
+        updatedWarehouse: rowResult.warehouse,
+        boxesRemoved: acc.boxesRemoved + rowResult.count,
+      }
+    },
+    { updatedWarehouse: currentWarehouse, boxesRemoved: 0 }
+  )
+
+  return boxesRemoved > 0 ? boxesRemoved + removeBoxes(updatedWarehouse) : 0
+}
+
 const part1 = (rawInput: string) => {
   return parseWarehouse(rawInput).reduce((total, row, rowIndex, warehouse) => {
     return total + row.reduce((rowTotal, item, colIndex) => {
@@ -59,9 +104,7 @@ const part1 = (rawInput: string) => {
 }
 
 const part2 = (rawInput: string) => {
-  const input = parseWarehouse(rawInput)
-
-  return
+  return removeBoxes(parseWarehouse(rawInput))
 }
 
 run({
@@ -85,10 +128,19 @@ run({
   },
   part2: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: `..@@.@@@@.
+@@@.@.@.@@
+@@@@@.@.@@
+@.@@@@..@.
+@@.@@@@.@@
+.@@@@@@@.@
+.@.@.@.@@@
+@.@@@.@@@@
+.@@@@@@@@.
+@.@.@@@.@.`,
+        expected: 43,
+      },
     ],
     solution: part2,
   },
